d860e922213d45c12e95dafcf866bb03322086f6
willian95 <rodriguezwillian95@gmail.com>
2021-06-14 15:34:22 -0400

    front server setupo

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -25,6 +25,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
