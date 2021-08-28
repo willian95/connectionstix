607a69890561e376da53ea7aefe47ea2e954a1b3
willian95 <rodriguezwillian95@gmail.com>
2021-08-28 11:06:14 -0400

    cambios

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -320,6 +320,8 @@ export default {
   },
   created() {
 
+    this.$i18n.setLocale("en")
+
     this.getConfig();
     if(process.browser){
       this.isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
